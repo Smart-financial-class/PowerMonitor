cf4eb4c89aa293370eebacbf1ee54018ae370a9d
HDU_Nbsp <1808347356@qq.com>
2022-09-13 16:29:35 +0800

    新建MainPage页面，并上传了MIT证书

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './views/MainPage.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <MainPage></MainPage>
 </template>
 
 <style scoped>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="page">Hey,MainPage</div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
+
+<style scoped>
+
+</style>
